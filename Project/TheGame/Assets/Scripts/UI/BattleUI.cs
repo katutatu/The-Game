2b7955801adfd62e0bce0581a1cd63955ae2b919
AKASAWAWONHIHUMI <ryo3prasu2@docomo.ne.jp>
2022-01-11 23:33:55 +0900

    Scriptsフォルダ追加

---
+++ Project/TheGame/Assets/Scripts/UI/BattleUI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleUI : MonoBehaviour
+{
+    private GameObject _hoge;
+    private GameObject _hoge2;
+
+
+    private void HOGE()
+    {
+        if (true)
+        {
+        }
+    }
+}
