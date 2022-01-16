423d6893247b8547fbfe155e8e53f84591bd7694
AKASAWAWONHIHUMI <ryo3prasu2@docomo.ne.jp>
2022-01-17 00:16:14 +0900

    カメラ処理調整

--- Project/TheGame/Assets/Scripts/Camera/BattleCamera.cs
+++ Project/TheGame/Assets/Scripts/Camera/BattleCamera.cs
@@ -17,16 +17,25 @@ public class BattleCamera : MonoBehaviour
     public void SetTargetPlane(IPlaneReadOnly targetPlane)
     {
         _targetPlane = targetPlane;
+        transform.position = GetGoalPosition();
     }
 
     private void Update()
     {
         if (_targetPlane != null)
         {
-            var goalPos = _targetPlane.Position + _offset;
-            var goalToCurrent = goalPos - transform.position;
-            var move = goalToCurrent * Time.deltaTime * 10.0f;
+            var currentToGoal = GetGoalPosition() - transform.position;
+            var move = currentToGoal * Time.deltaTime * 10.0f;
             transform.position += move;
         }
+    }
+
+    private Vector3 GetGoalPosition()
+    {
+        if (_targetPlane == null)
+        {
+            return Vector3.zero;
+        }
+        return _targetPlane.Position + _offset;
     }
 }
